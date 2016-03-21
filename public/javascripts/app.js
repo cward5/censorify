a3bdbb23c14798a5cdf5a39720fa08273c5228ab
Chris Ward <marcgyven@gmail.com>
2016-03-21 23:42:05 +0000

    Lab 9

---
+++ public/javascripts/app.js
@@ -0,0 +1,62 @@
+angular.module('comment', [])
+.controller('MainCtrl', [
+  '$scope', '$http',
+  function($scope,$http){
+    $scope.test = 'Hello world!';
+    $scope.comments = [
+
+    ];
+    $scope.addComment = function() {
+      if($scope.formContent === '') { return; }
+      console.log("In addComment with "+$scope.formContent);
+      $scope.create({
+        title: $scope.formContent,
+        upvotes: 0,
+      });
+      $scope.formContent = '';
+    };
+    $scope.incrementUpvotes = function(comment) {
+      $scope.upvote(comment);
+    };
+    $scope.decrementUpvotes = function(comment) {
+      $scope.downvote(comment);
+    };
+    $scope.getAll = function() {
+      return $http.get('/comments').success(function(data){
+        angular.copy(data, $scope.comments);
+	$scope.filter();
+	console.log($scope.comments);
+      });
+    };
+    $scope.create = function(comment) {
+      return $http.post('/comments', comment).success(function(data){
+        $scope.comments.push(data);
+      });
+    };
+    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+      .success(function(data){
+        console.log("upvote worked");
+        comment.upvotes += 1;
+        $scope.getAll();
+      });
+    };
+    $scope.downvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/downvote')
+      .success(function(data){
+        console.log("downvote worked");
+        comment.upvotes -= 1;
+        $scope.getAll();
+      });
+    };  
+    $scope.filter = function() {
+      for(var i=0;i<$scope.comments.length;i++) {
+        if($scope.comments[i].upvotes<0)
+          {
+            $scope.comments[i].title="Comment falls beneath viewing threshold"
+          };
+      };
+    };
+    $scope.getAll();
+  }
+]);
